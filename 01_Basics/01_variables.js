ec312602b77925e7570d9394defc1bec0a5a19be
radhika yadav <radhikayaduvanshi001@gmail.com>
2024-07-17 18:55:36 +0530

    01_variables.js  How to declare the variable

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+//Cannot change the value
+const accountId=1223;
+
+//can change the value at any time
+let  accountName="something";
+
+//cannot use out of the scope
+var  accountPassword=238033;
+
+//mostly used to declare a variables : const and let 
