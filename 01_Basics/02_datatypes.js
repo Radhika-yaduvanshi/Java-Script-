73af6e391d08ab1b869911f01995b070223089a2
radhika yadav <radhikayaduvanshi001@gmail.com>
2024-07-17 19:38:03 +0530

    Datatypes in Java Script

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treats all js code as newer version  or in a mordern way 
+
+// alert(3+3)   will throw error bcz we are using node.js not browser
+
+let name="radhika" //String
+let age=21//integer
+// bigint;  used for big size of integer value
+// Boolean   true/false
+let temprature = null  // value is not arrived  or not exists
+
+let bdate=undefined  //  can modify the field latter
+
+//symbole =>unique
+
+//object 
+
+console.log(typeof name);//string
+console.log(typeof age);//number
+console.log(typeof temprature);  //null==>type : object
+console.log(typeof bdate);//undefined
