4becafbf5b616b6e3eb13545ceb81f92a44e82c4
radhika yadav <radhikayaduvanshi001@gmail.com>
2024-07-17 21:29:53 +0530

    string and its functions in js

---
+++ 05_String.js
@@ -0,0 +1,35 @@
+let name="radhika";
+let age=21
+
+console.log(`hello my name is ${name} and my age is ${age}`);
+console.log(name[0]);
+
+let name1= new String("radhika");
+console.log(name1[0]);
+console.log(name1.charAt(3));
+console.log("index of 3 is "+name1.indexOf('h'));
+console.log(name1.toUpperCase());
+// console.log(name1.toUpperCase());
+
+let newstring=  name1.substring(0,4);
+console.log(newstring);
+
+
+let str="       hi   radhika :) x";
+console.log(str.trim());
+
+let url="https://www.google.com";
+
+let nice =url.replace("google","amezone");
+console.log(url);
+console.log(nice);
+console.log(url.includes("nice"));
+console.log(name.split('/'));
+
+
+
+
+
+
+
+
