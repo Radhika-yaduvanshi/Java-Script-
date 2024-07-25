e3e725c140482779772109f875233b685165fe7a
radhika yadav <radhikayaduvanshi001@gmail.com>
2024-07-26 00:02:07 +0530

    Update 01_functions.js
    Rest operator

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,3 +33,35 @@ function loginuser(username)
 }
 
 console.log(loginuser("Radhika"))
+
+
+
+//rest operator  [...]
+function calculateCartPrice(val1,val2,...num1){
+return num1
+}
+
+console.log(calculateCartPrice(2000,3000,4000))
+
+const user1={
+    username:"radhika",
+    price:199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user1)
+
+// another way to print the or passing the data .
+handleObject({username:"naina",price:90000})
+
+const myNewArray =[100,200,300,400]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray))
+
